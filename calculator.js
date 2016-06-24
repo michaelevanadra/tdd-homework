87baaced334763bef0237be84eca1709b3ee22dc
Michael Evan Adra <adram@ph.ibm.com>
2016-06-24 21:57:25 +0800

    1. Make the first test pass (should add two numbers).

--- calculator.js
+++ calculator.js
@@ -1,7 +1,10 @@
 'use strict';
 
-exports.add = function() {
-  // your code here.
+/**
+ * A function the returns the sum of two Numbers
+ */
+exports.add = function( val1, val2 ) {
+  return Number(val1) + Number(val2);
 }
 
 exports.multiply = function(val1, val2) {
