f02d2de1f83350d039beacf667a0e924d436901f
Michael Evan Adra <adram@ph.ibm.com>
2016-06-24 22:03:11 +0800

    2. Create the test case for should multiply two numbers.

--- calculator.test.js
+++ calculator.test.js
@@ -5,17 +5,20 @@ var assert = require('assert');
 var calculator = require('./calculator');
 
 describe('Calculator', function() {
-  
+
   it('should add two numbers', function() {
     assert.equal(calculator.add(1,1), 2);
   });
-  
+
   it('should multiply two numbers', function() {
-    // your code here
+    assert.equal(calculator.multiply(1,0), 0);
+    assert.equal(calculator.multiply(1,1), 1);
+    assert.equal(calculator.multiply(1,2), 2);
+    assert.equal(calculator.multiply(2,3), 6);
   });
-  
+
   // test for divide
-  
+
   // test of remainder
-  
+
 });
