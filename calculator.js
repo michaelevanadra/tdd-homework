e518428e5b28f6e336d6c8cd23f19f76090be940
Michael Evan Adra <adram@ph.ibm.com>
2016-06-24 22:08:56 +0800

    4. Make the remainder test pass

--- calculator.js
+++ calculator.js
@@ -13,4 +13,9 @@ exports.multiply = function(val1, val2) {
 
 // behavior for divide
 
-// bahavior for remainder
+/**
+ * Function that accepts to number and returns the remainder
+ */
+exports.remainder = function( val1, val2 ){
+  return Number( val1 ) % Number( val2 );
+}
